868ad7cf954aef1dae913f138294f26cc6514e9e
Filipe Melo de Araujo <filipemelomelo131@gmail.com>
2023-01-23 16:59:39 -0300

    Update cleaning_and_security.js

--- cleaning_and_security.js
+++ cleaning_and_security.js
@@ -2,15 +2,15 @@ function cleaning_and_security(){
 
   var spreadsheet = SpreadsheetApp.getActive()
 
-  var predio = spreadsheet.getRange('D2')
-  var hora = spreadsheet.getRange('D3')
-  var dias =  spreadsheet.getRange('D4')
+  var predio = spreadsheet.getSheetByName('Simulação').getRange('D2').getValue()
+  var hora = spreadsheet.getSheetByName('Simulação').getRange('D3').getValue()
+  var dias =  spreadsheet.getSheetByName('Simulação').getRange('D4').getValue()
 
-  var custos_de_limpeza = spreadsheet.getRange('G4')
-  var custos_de_seguranca = spreadsheet.getRange('G5')
+  var custos_de_limpeza = spreadsheet.getSheetByName('Simulação').getRange('G4')
+  var custos_de_seguranca = spreadsheet.getSheetByName('Simulação').getRange('G5')
 
-  vh_limpeza = (3771.05 / (12 * 24)) * (hora * dias)
-  vh_seguranca = (19084.60 / (12 * 24)) * (hora * dias)
+  vh_limpeza = (3771.05 / (30 * 24)) * (hora * dias)
+  vh_seguranca = (19084.60 / (30 * 24)) * (hora * dias)
 
   var ordem = new Map()
   ordem.set('Campo de Futebol', 6)
@@ -20,5 +20,4 @@ function cleaning_and_security(){
 
   custo_de_limpeza.setValue(valor_limpeza*ordem.get(predio))
   custo_de_seguranca.setValue(valor_seguranca)
-
 }
